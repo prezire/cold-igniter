dfc103e41aa56ecb2330dc265d1d21945508a054
Mark Amoin <prezire@gmail.com>
2015-04-12 23:56:44 +0800

    Removed generated CRUD codes.

--- application/third_party/generators/cruds/position_alerts_20150410111931/position_alerts_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE position_alerts(id INT NOT NULL AUTO_INCREMENT,
-email VARCHAR(255) NOT NULL,
-frequency ENUM('Daily','Weekly') NOT NULL,
-keywords VARCHAR(255) NOT NULL,
-date_time_created DATETIME NOT NULL,
-PRIMARY KEY (id))
--- application/third_party/generators/cruds/position_alerts_20150410112845/position_alerts_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE position_alerts(id INT NOT NULL AUTO_INCREMENT,
-email VARCHAR(255) NOT NULL,
-frequency ENUM('Weekly','Monthly') NOT NULL,
-keywords VARCHAR(255) NOT NULL,
-country VARCHAR(255) NOT NULL,
-date_time_created DATETIME NOT NULL,
-PRIMARY KEY (id))
